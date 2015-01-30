4a21695828ad3e8fb1bd2b2a596d3b310185e17b
Nicolas Higuera <nicohiguera11@gmail.com>
2015-01-29 16:00:45 -0800

    weather clock

--- js/clock.js
+++ js/clock.js
@@ -4,3 +4,31 @@ function getTime () {
 }
 
 getTime();
+
+function getTemp() {
+  var url = 'https://api.forecast.io/forecast/277e7a349fc051c5a3a8169819b178aa/35.300399,-120.662362?callback=?';
+
+  console.log("getting it");
+	$.getJSON(url, function getTempCallback(data){
+
+    $('#forecastIcon').html('<img src="img/' + data.daily.icon + '.jpg">');
+    $('#forecastLabel').html(data.daily.summary);
+
+    $('body').addClass(function getTempClass(){
+      var temp = data.daily.data[0].temperatureMax;
+      
+      if (temp < 60)
+        return 'cold';
+      else if (temp < 70)
+        return 'chilly';
+      else if (temp < 80)
+        return 'nice';
+      else if (temp < 90)
+        return 'warm';
+      else
+        return 'hot';
+    });
+  });
+}
+
+getTemp();
